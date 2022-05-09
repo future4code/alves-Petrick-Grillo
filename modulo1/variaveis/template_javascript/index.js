039699b58c812bfe407a0de4fec3a05ce4072e83
Petrick Grillo <petrick.grillo@gmail.com>
2022-05-09 17:07:05 -0300

    1 aula Java Script

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,31 @@
+// 1- Sera impresso o valor de a=10 e b=5 
+
+// 2 - Sera impresso o valor de a=20 b=20 e c=20
+
+/* 3 - Melhores nomes para as variaveis seriam 
+let horasDia = prompt...
+let salarioDia = prompt...
+alert ("voce recebe ${horasDia/salarioDia} por hora")*/
+
+let nome
+let idade
+typeof nome
+typeof idade
+console.log(typeof nome) 
+console.log(typeof idade)
+// Este tipo foi impresso pois nao possui atribuição e nem um valor
+
+const pergNome = prompt ("Qual é seu nome?")
+const pergIdade = prompt("Qual é sua idade?")
+console.log(typeof pergIdade,typeof pergNome)
+// Fez uma pergunta para o usuartesio, me dando informações para que efetue o proximo comando
+
+console.log("Olá", pergNome, "voce tem", pergIdade, "anos")
+
+// Exercicio 2
+
+let sim = true
+let não = false 
+
+const pergAlmoco = prompt 
+
