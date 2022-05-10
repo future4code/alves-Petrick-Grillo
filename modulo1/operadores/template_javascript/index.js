d4a76d6b635a1c5dd3419fbfa4febdecb785a5b9
Petrick Grillo <petrick.grillo@gmail.com>
2022-05-10 16:54:20 -0300

    Aula operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,82 @@
+// **Exercicios de interpretação**
+// 1 - 
+// console a = false
+
+// console b = false 
+
+// console c = false
+
+// console d = typeof string 
+
+// 2 -
+// A sugestao para solucionar o problema é de que a declaração  da variavel soma seja let,
+// visto que o resultado final da variavel soma ira mudar de acordo com a resposta do usuario.
+// ===========================================================================================================
+
+//**Exercicios de escrita de codigo
+// 1 - 
+// const pergIdadeuser = prompt ("Qual é sua idade?")
+
+// const pergIdadeamig = prompt ("Qual é a idade do seu melhor amigo(a)?")
+
+// let somaIdade = pergIdadeuser > pergIdadeamig
+
+// console.log("Sua idade é maior que a do seu amigo?", somaIdade)
+// console.log("A diferença de idade entre vocês é de", pergIdadeuser - pergIdadeamig,"anos") 
+
+// 2- 
+//  const pergNumeropar = prompt ("Insira um numero par")
+//  console.log(pergNumeropar%2)
+// O padrao que sempre se repete é de que nunca falta/sobrara um numero
+// caso o numero inserido seja par
+// Quando o usuario inserir um numero que nao for par sempre sobrara 1
+// visto que esta sendo divido por 2
+
+// 3 -
+// const pergIdadeusu = prompt ("Qual é sua idade em anos?")
+// let idadeMeses = pergIdadeusu * 12
+// let idadeDias = pergIdadeusu * 365
+// let idadeHoras = idadeDias * 24
+// console.log("Sua idade em meses é", idadeMeses)
+// console.log("Sua idade em dias é", idadeDias)
+// console.log("Sua idade em horas é", idadeHoras)
+
+// 4- 
+
+// const numeroUm = prompt ("Escolha um numero inteiro")
+// const numeroDois = prompt ("Escolha outro numero inteiro")
+
+// let primeiroMaior = numeroUm > numeroDois
+// console.log("O primeiro numero é maior que o segundo?", primeiroMaior)
+
+// let primeiroIgual = numeroUm === numeroDois
+// console.log("O primeiro numero é igual ao segundo?", primeiroIgual)
+
+// let primeiroDivisivel = numeroUm <= numeroDois
+// console.log("O primeiro numero é divisivel pelo segundo?", primeiroDivisivel)
+
+// let segundoDivisivel = numeroDois <= numeroUm
+// console.log("O Segundo numero é divisivel pelo primeiro?", segundoDivisivel)
+// =============================================================================================
+// Desafio
+// 1- 
+
+// let perguntaFah = prompt ("Qual é a temperatura em Fahrenheit que deseja tranformar para Kelvin?")
+// let valorFparaK = (perguntaFah - 32) * (5/9) + 273.15
+// console.log("A temperatura em Kelvin é de", valorFparaK) //298,15
+
+// let perguntaCel = prompt ("Qual é a temperatura em Graus Celsius que deseja transformar em Fahrenheit?")
+// let valorCparaF = (perguntaCel)*(9/5) + 32
+// console.log("A temperatura em Fahrenheit é de", valorCparaF) //176
+
+// let perguntaDupla = Number(prompt ("Qual é a temperatura em Graus Celsius que deseja transformar em Fahrenheit e Kelvin?"))
+// let valorCparaFf = (perguntaDupla)*(9/5) + 32
+// let valorCParaK = (valorCparaFf - 32) * (5/9) + 273.15
+// console.log("Sua temperatura em Fain e Kelvin são", valorCparaFf, valorCParaK) //Fah 86 e K 303,15
+
+
+
+
+
+
+
