9ca1cc2379cd9207ef09a999534d5e9bbe308bdd
Petrick Grillo <petrick.grillo@gmail.com>
2022-05-11 17:04:16 -0300

    Strings-arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,43 @@
+// Exercicios de interpretação
+// 1- 
+// a - erro 
+// b - Nada 
+// c - 11
+// d - 0
+// e - 1
+// f - 8
+// 
+// 2 - "SUBI NUM ONIBUS EM MIRROCOS", 5
+//=====================================================================================
+
+// Exercicios de escrita de codigo
+// 1 - 
+// const nomeDoUsuario = prompt ("Qual é seu nome?")
+
+// const emailDoUsuario = prompt ("Qual é seu email?")
+
+// console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vinda(o),", nomeDoUsuario,"!") 
+ 
+// 2 -
+
+// const nomeComidas='Arroz\n Feijao \n Farora \n Suco \n Carne'
+
+// console.log('Essas sao minhas comidas prefereidas:\n',nomeComidas)
+//
+//-Desafio c:
+// const comidaUsuario = prompt (["Qual é sua comida favorita?"])
+// const nomeComidas =["Arroz", "Feijao","Farora", "Suco", "Carne"]
+// nomeComidas.splice(1,1)
+// nomeComidas.push(comidaUsuario)
+// console.log(nomeComidas)
+// 3 -
+// const listaDeTarefas = []
+// perguntaTarefa1 = prompt ("1º Tarefa do dia?")
+// perguntaTarefa2 = prompt ("2º Tarefa do dia?")
+// perguntaTarefa3= prompt ("3º Tarefa do dia?")
+// const array = [perguntaTarefa1, perguntaTarefa2, perguntaTarefa3]
+// console.log(array)
+// const perguntaTirarTarefa = prompt ("Qual tarefa voce ja concluiu?")
+// array.splice (perguntaTirarTarefa - 1, 1)
+// console.log(array)
+
