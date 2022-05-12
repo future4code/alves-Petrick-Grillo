23f75d115b307a30f94e185b163de74a412c781d
Petrick Grillo <petrick.grillo@gmail.com>
2022-05-12 19:13:01 -0300

    Funções feitas

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -25,23 +25,69 @@
 
 //1 - b
 
-const exibeMsg = (nome,idade,cidade,profissao) => {
-        return 'Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.'
-    }
-    const resultado = exibeMsg("Pétrick", 18, "Araraquara", "Estudante")
+// let exibeMsg = (nome,idade,cidade,profissao) => {
+//         return `Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e sou ${profissao}.`
+        
+//     }
+//     const resultado = exibeMsg ("Pétrick", 18, "Araraquara","Estudante")
     
-    console.log()
+//     console.log (resultado)
+
+// 2 -
+// A)
+
+// let algo = (num1,num2) => {
+//     let soma = num1 + num2
+//     return soma
+// }
+//  let resultadoSoma = algo(4, 3)
+//  console.log(resultadoSoma)
+
+// 2 - 
+// B)
+
+// let somaBol = (num1, num2) => {
+//     let verificar = num1 >= num2
+//     return verificar
+// }
+//     let resultadoNum.pushBol(4,5)
+//     console.log(resultadoNum)
     
+//C)
+//  let descobrirBol = (num1) => {
+//      let contaBol = (num1%2)
+//      contaBol = (0 === 0)
+//      return contaBol 
+//  } 
+// let verificarBol = descobrirBol(2)
 
+// console.log(verificarBol)
 
+//D)
+//
+// let msgAle = (msg) => {
+//     let aumentarMsg = msg.toUpperCase()
+//     console.log(aumentarMsg)
+//     let contarMsg = msg.length
+//     return contarMsg
+// }
+// let impressaoMsg = msgAle("tEste")
+// console.log(impressaoMsg)
 
+// 3 - Crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão). 
+// Em seguida, peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário
+// sendo o argumento. Por fim, mostre no console o resultado das operações:
 
-
-
-
-
-
-
+// let funcAle =(num1,num2) => {
+//     let soma = []
+//     soma.push (num1+num2)
+//     soma.push (num1-num2)
+//     soma.push (num1/num2)
+//     soma.push (num1*num2)
+//     return soma
+// }
+// let resultadoFinal = funcAle(4,4)
+// console.log(resultadoFinal)
 
 
 
