33ee1c2eb233d0c62c181abb228ff69f60c50288
Petrick Grillo <petrick.grillo@gmail.com>
2022-05-18 17:17:34 -0300

    Laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,39 @@
+// EXERCICIOS DE INTERPRETAÇÃO DE CODIGO
+// 1 - O codigo abaixo esta listando os valores que forem de 0 ate abaixo de 5, o resultado impresso no console sera de 10
+// 2 - A) Sera impresso no console todo numero que for maior que 18 na variavel lista B) colocar o .length no console
+// 3 - Sera de 1,, fazendo com que defina a quantidade de * para cada quantidadeTotal que o usuario colocar
+
+// EXERCICIOS DE ESCRITA DE CODIGO
+// 1 - 
+
+// let perguntaUsuario = +prompt("Quantos animais de estimação você tem?")
+// let adicaoAnimal = ("")
+
+//     if (perguntaUsuario === 0){
+    
+//     console.log("Que pena! Você pode adotar um pet!")
+//     } else if(perguntaUsuario>0) {
+//         let nomeDoAnimal = []
+//         while (nomeDoAnimal.length < perguntaUsuario){
+//         let nomeDosAnimais = prompt("Quais sao os nomes deles?")
+//         nomeDoAnimal.push(nomeDosAnimais)   
+//     }
+//     console.log(nomeDoAnimal)
+//     }
+        
+    let perguntaUsuario = +prompt("Quantos animais de estimação você tem?")
+    let adicaoAnimal = ("")
+    let numeroPergunta = perguntaUsuario
+    let inicializador = 0
+
+        while(perguntaUsuario !== 0){
+        }
+        console.log("Que pena! Voce pode adotar um pet!")
+        while (inicializador<perguntaUsuario){
+            NomedoAnimal = prompt ("Quais sao os nomes deles?")
+            NomedoAnimal = []
+            exibirNomes = NomedoAnimal.push([0])
+            exibirNomes += adicaoAnimal
+        }
+console.log(adicaoAnimal)
+
