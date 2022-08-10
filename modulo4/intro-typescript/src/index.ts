a25389e7fcdd62d8f98314bcc3dcc2f47e2d2bcb
Petrick Grillo <petrick.grillo@gmail.com>
2022-08-10 17:12:57 -0300

    Intro Typescript

---
+++ modulo4/intro-typescript/src/index.ts
@@ -0,0 +1,19 @@
+// const a = process.argv[2]
+// const b = process.argv[3]
+// const c = process.argv[4]
+
+// function checaTriangulo() {
+//     if (a !== b && b !== c) {
+//         return "Escaleno";
+//     } else if (a === b && b === c) {
+//         return "Equilátero";
+//     } else {
+//         return "Isósceles";
+//     }
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// console.log(checaTriangulo)
