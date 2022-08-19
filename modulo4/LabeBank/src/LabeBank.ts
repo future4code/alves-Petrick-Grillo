88b726db4149be6343e40e083b02843284a7252b
Petrick Grillo <petrick.grillo@gmail.com>
2022-08-19 20:38:35 -0300

    falta ultimo

--- modulo4/LabeBank/src/LabeBank.ts
+++ modulo4/LabeBank/src/LabeBank.ts
@@ -8,7 +8,7 @@ type users = {
     nome: string
     CPF: number | string
     nascimento: Date | string
-    extrato: extrato
+    extrato: extrato[]
 }
 type extrato = {
     saldo: number
@@ -26,38 +26,22 @@ let users: users[] = [{
     nome: "Usuario 01",
     CPF: "101.010.100.11",
     nascimento: "11/01/2001",
-    extrato: {
-        saldo: 0,
-        data: "",
-        descricao: ""
-    }
+    extrato: []
 }, {
     nome: "Usuario 02",
     CPF: "202.020.200.22",
     nascimento: "22/02/2002",
-    extrato: {
-        saldo: 0,
-        data: "",
-        descricao: ""
-    }
+    extrato: []
 }, {
     nome: "Usuario 03",
     CPF: "303.030.300.33",
     nascimento: "33/03/3003",
-    extrato: {
-        saldo: 0,
-        data: "",
-        descricao: ""
-    }
+    extrato: []
 }, {
     nome: "Usuario 04",
     CPF: "404.040.400.44",
     nascimento: "44/04/4004",
-    extrato: {
-        saldo: 0,
-        data: "",
-        descricao: ""
-    }
+    extrato: []
 }]
 
 
@@ -97,11 +81,7 @@ exercicios.post("/addUser", (req: Request, res: Response) => {
             nome: req.body.nome,
             CPF: req.body.CPF,
             nascimento: req.body.nascimento,
-            extrato: {
-                saldo: 0,
-                data: "",
-                descricao: ""
-            }
+            extrato: []
         }
         users.push(addUser)
         res.send({ "Adicionado!": addUser })
@@ -130,10 +110,48 @@ exercicios.patch("/searchUser/:cpf/", (req: Request, res: Response) => {
         descricao: req.body.descricao
     }
     const searchUser = users.findIndex(parametro => parametro.CPF === cpf && parametro.nome === nameUser)
-    users[searchUser].extrato = saldo
+    users[searchUser].extrato.push(saldo)
     res.status(200).send({ "Deposito feito com sucesso!": users })
 })
-
+exercicios.post("/User/:cpf/payment", (req: Request, res: Response) => {
+    let dataAtual = new Date
+    const anoAtual = dataAtual.getFullYear()
+    const mesAtual = dataAtual.getMonth() + 1
+    const diaAtual = dataAtual.getDate()
+    let dataAtualFormatada = diaAtual + "/" + mesAtual + "/ " + anoAtual
+    const cpf = req.params.cpf
+    const value = req.body.saldo
+    const description = req.body.descricao
+    console.log(value, description)
+    let data = req.body
+    const searchUser = users.findIndex(parametro => parametro.CPF === cpf)
+    console.log(searchUser)
+    try {
+        if (!data) {
+            data = dataAtualFormatada
+        }
+        if (!value || !description) {
+            res.status(res.statusCode).send("Erro, esta faltando algum parametro!")
+        }
+        if (searchUser < 0) {
+            res.status(res.statusCode).send("Usuario não encontrado!")
+        }
+        const usuario: users = users[searchUser]
+        const transacaoUser: extrato = {
+            saldo: value,
+            data: data,
+            descricao: description
+        }
+        if (Math.abs(value) > +(usuario.extrato)) {
+            res.status(res.statusCode).send("Saldo insuficiente!")
+        }
+        usuario.extrato.push(transacaoUser)
+        console.log(usuario)
+    }
+    catch (error: any) {
+        res.status(res.statusCode).send(error.message)
+    }
+})
 
 exercicios.listen(3003, () => {
     console.log("LabeBank rodando 3003!")
