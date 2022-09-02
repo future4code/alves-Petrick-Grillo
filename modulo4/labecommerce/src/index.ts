4cc664593bda80bce37551a72fdccf56f94b0401
Petrick Grillo <petrick.grillo@gmail.com>
2022-09-02 15:30:41 -0300

    Finalizado

--- modulo4/labecommerce/src/index.ts
+++ modulo4/labecommerce/src/index.ts
@@ -79,28 +79,17 @@ const getAllProducts = async (req: Request, res: Response) => {
         res.status(500).send(`Aconteceu um erro inesperado`)
     }
 }
-const baseURL = "http://localhost:3003"
-const getProductbyID = async (req: Request, res: Response) => {
-    try {
-        const id = req.params.id
-        const allProducts = await connection("labecommerce_products")
-            .select("*")
-            .where("id", id)
-        res.status(200).send(allProducts)
-    } catch (erro) {
-        console.log(erro);
-        res.status(500).send(`Aconteceu um erro inesperado`)
-    }
+const getProductbyID = async (id: string) => {
+    const allProducts = await connection("labecommerce_products")
+        .select("*")
+        .where("id", id)
+    return allProducts
 }
-
-const getProduct = async (idProduct: any): Promise<any> => {
-    try {
-        const api = await axios.get(`${baseURL}/products/${idProduct}`)
-        console.log(api)
-        return api.data
-    } catch (erro) {
-        throw new Error();
-    }
+const getUserbyID = async (id: string) => {
+    const allProducts = await connection("labecommerce_users")
+        .select("*")
+        .where("id", id)
+    return allProducts
 }
 const createBuyList = async (req: Request, res: Response) => {
     try {
@@ -109,18 +98,66 @@ const createBuyList = async (req: Request, res: Response) => {
             product_id: req.body.product_id,
             quantity: req.body.quantity
         }
-        const userProd = await getProduct(prod.product_id)
-        console.log(prod)
-        const somaProduc = userProd * prod.quantity
+        const userProd: any = await getProductbyID(prod.product_id)
+        const somaProduc = userProd[0].price * prod.quantity
         if (!prod.user_id || !prod.product_id || !prod.quantity) {
             res.status(400).send("Preencha todos os campos!")
         }
-        await connection("labecommerce_products")
-            .insert(prod)
-        res.status(200).send({ prod, somaProduc, message: "Produto criado com sucesso" })
+        await connection("labecommerce_purchases")
+            .insert({
+                user_id: prod.user_id,
+                product_id: prod.product_id,
+                quantity: prod.quantity,
+                total_price: somaProduc
+            })
+        res.status(200).send({ prod, message: "Produto criado com sucesso" })
     } catch (erro) {
         console.log(erro);
         res.status(500).send(`Aconteceu um erro inesperado`)
+    }
+}
+const selectPurchasesByUser = async (id: string) => {
+    const result = await connection.raw(`
+    select labecommerce_users.id as IdPerson, labecommerce_users.name as NamePerson,labecommerce_products.name as NameProduct, labecommerce_purchases.id as IdCompra, labecommerce_purchases.quantity, labecommerce_purchases.total_price
+    from labecommerce_purchases
+    inner join labecommerce_users on
+    labecommerce_purchases.user_id = labecommerce_users.id
+    inner join labecommerce_products on
+    labecommerce_purchases.product_id = labecommerce_products.id
+    where labecommerce_users.id = "${id}";
+`)
+    let infoUser: any = {}
+    let Allpurchases: any[] = []
+    result[0].map((person: any) => {
+        infoUser = {
+            idPerson: person.IdPerson,
+            NamePerson: person.NamePerson
+        }
+        Allpurchases.push({
+            idPurchase: person.IdCompra,
+            ProductName: person.NameProduct,
+            Quantity: person.quantity,
+            TotalPrice: person.total_price
+        })
+    })
+    const allPurchasesByUser = {
+        idCompra: infoUser.idPerson,
+        NamePerson: infoUser.NamePerson,
+        infoPurchaseByUser: Allpurchases
+    }
+    return allPurchasesByUser
+}
+const getHistory = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const userId = req.params.userId
+        const userCollected = await getUserbyID(userId)
+        if (!userCollected.length) {
+            res.status(400).send(`Usuario com o id ${userId} não foi encontrado!`)
+        }
+        const History = await selectPurchasesByUser(userId)
+        res.status(200).send(History)
+    } catch (erro: any) {
+        res.status(500).send({ message: erro.message })
     }
 }
 
@@ -128,6 +165,7 @@ const createBuyList = async (req: Request, res: Response) => {
 app.get("/users", getAllUser)
 app.get("/products", getAllProducts)
 app.get("/products/:id", getProductbyID)
+app.get("/users/:userId/purchases", getHistory)
 app.post("/purchases", createBuyList)
 app.post("/users", addUser)
 app.post("/products", regisProduct)
